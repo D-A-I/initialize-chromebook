87b56b75925b15ca68f91d7602582cc19b7aa9d6
i-a-d <you@example.com>
2021-12-26 23:49:14 +0900

    add init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,74 @@
+; emacs - basic settings
+
+; package-archives
+(require 'package)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("org" . "https://orgmode.org/elpa/")
+        ("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+
+; ----
+; language
+; ----
+;; set language as Japanese
+(set-language-environment 'Japanese)
+;; coding UTF8
+(set-language-environment 'utf-8)
+(prefer-coding-system 'utf-8)
+; ----------------
+; key bind
+; ----------------
+;; C-h
+(global-set-key "\C-h" 'delete-backward-char)
+;; help
+(global-set-key "\C-c\C-h" 'help-command)
+;; comment out
+(global-set-key "\C-c;" 'comment-dwim)
+;; window operation
+(global-set-key "\C-t" 'other-window)
+; ----
+; preferences
+; ----
+; 括弧を自動で補完する
+(electric-pair-mode 1)
+;; tabにスペース４つを利用
+(setq-default tab-width 4 indent-tabs-mode nil)
+;; デフォルトの起動時のメッセージを表示しない
+(setq inhibit-startup-message t)
+;; 列の番号
+(column-number-mode t)
+;; 行番号の表示
+(global-linum-mode t)
+;; 1行ごとの改ページ
+(setq scroll-conservatively 1)
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+;; メニューバーの非表示
+(menu-bar-mode -1)
+;; ツールバーの非表示
+(tool-bar-mode -1)
+
+; +++ ここからカスタマイズ部.. 慎重にメンテする +++
+
+;; MigMix
+(add-to-list 'default-frame-alist '(font . "MigMix 1M"))
+
+;; night-owl-theme
+(load-theme 'night-owl t)
+
+;; powerline
+(powerline-default-theme)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(night-owl-theme powerline)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
