b38a6d0a35b8fed8f4b1458ece9e62168742f022
i-a-d <i-a-d@example.com>
2021-12-28 23:12:54 +0900

    リファクタリング

--- provisioning.sh
+++ provisioning.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ux
 
-echo "+-+-+-+ start setup. +-+-+-+"
+echo "-+-+-+- start setup. -+-+-+-"
 
 # package update
 
@@ -9,7 +9,7 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y dist-upgrade
 
-echo "+-+-+-+ package updated complete. +-+-+-+"
+echo "-+-+-+- package updated complete. -+-+-+-"
 
 # localization
 
@@ -20,12 +20,11 @@ sudo apt -y install \
 
 sudo localectl set-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 . /etc/default/locale
-
 ## timezone
 sudo ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime &&
   sudo echo Asia/Tokyo >/etc/timezone
 
-echo "+-+-+-+ localization complete. +-+-+-+"
+echo "-+-+-+- localization complete. -+-+-+-"
 
 # IME
 
@@ -37,14 +36,13 @@ if [ ! -e ${_TEMP_MOZC_VAL} ]; then
   echo "file not exists. stop the process.."
   exit 1
 fi
-
 sudo cat <<EOF >>${_TEMP_MOZC_VAL}
 Environment="GTK_IM_MODULE=fcitx"
 Environment="QT_IM_MODULE=fcitx"
 Environment="XMODIFIERS=@im=fcitx"
 EOF
 
-echo "+-+-+-+ mozc configuration complete. +-+-+-+"
+echo "-+-+-+- mozc configuration complete. -+-+-+-"
 
 # auto-start IME
 
@@ -54,29 +52,25 @@ if [ ! -e ${_TEMP_SOMMELIERRC_VAL} ]; then
   echo "file not exists. stop the process.."
   exit 1
 fi
-
 echo "/usr/bin/fcitx-autostart" >>${_TEMP_SOMMELIERRC_VAL}
 
-echo "+-+-+-+ ime configuration complete. +-+-+-+"
+echo "-+-+-+- ime configuration complete. -+-+-+-"
 
 # default key bindings configuration
 
 readonly _TEMP_GTK3_VAL=~/.config/gtk-3.0/settings.ini
 _TEMP_GTK3_DIR_VAL=$(dirname ${_TEMP_GTK3_VAL})
 readonly _TEMP_GTK3_DIR_VAL
-
 ## If you don't have a setting.ini, create one.
 [ ! -e "${_TEMP_GTK3_DIR_VAL}" ] && mkdir -p "${_TEMP_GTK3_DIR_VAL}"
 [ ! -e ${_TEMP_GTK3_VAL} ] && touch ${_TEMP_GTK3_VAL}
-
 cat <<-EOF >>${_TEMP_GTK3_VAL}
 [Settings]
 gtk-key-theme-name = Emacs
 EOF
-
 gsettings set org.gnome.desktop.interface gtk-key-theme "Emacs"
 
-echo "+-+-+-+ default key bindings configuration complete. +-+-+-+"
+echo "-+-+-+- default key bindings configuration complete. -+-+-+-"
 
 # tools
 
@@ -90,7 +84,7 @@ sudo apt -y install \
   git \
   vivaldi-stable
 
-echo "+-+-+-+ tools installation complete. +-+-+-+"
+echo "-+-+-+- tools installation complete. -+-+-+-"
 
 # place init.el (~/.emacs.d/init.el)
 
@@ -98,12 +92,11 @@ readonly _TEMP_DOTFILE_VAL=https://raw.githubusercontent.com/D-A-I/initialize-ch
 readonly _TEMP_EMACS_VAL=~/.emacs.d/init.el
 _TEMP_EMACS_DIR_VAL=$(dirname ${_TEMP_EMACS_VAL})
 readonly _TEMP_EMACS_DIR_VAL
-
 ## If you don't have a init.el, create one.
 [ ! -e "${_TEMP_EMACS_DIR_VAL}" ] && mkdir -p "${_TEMP_EMACS_DIR_VAL}"
 [ ! -e ${_TEMP_EMACS_VAL} ] && touch ${_TEMP_EMACS_VAL}
 curl ${_TEMP_DOTFILE_VAL} > ${_TEMP_EMACS_VAL}
 
-echo "+-+-+-+ emacs configuration complete. +-+-+-+"
+echo "-+-+-+- emacs configuration complete. -+-+-+-"
+echo "-+-+-+- please reboot. use fcitx-configtool. -+-+-+-"
 
-echo "+-+-+-+ please reboot. +-+-+-+"
